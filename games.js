a60795ed37671ba1d0b9bd0d5d6f962e1a5dd6de
shanmukhathebest <Shanmukh.puvvada@huttoisd.org>
2025-10-04 12:45:01 -0500

    Create games.js

---
+++ games.js
@@ -0,0 +1,27 @@
+// List of open-source HTML5 games to embed/play
+const games = [
+  {
+    title: "2048",
+    description: "Classic tile merging puzzle.",
+    thumbnail: "https://play2048.co/favicon.ico",
+    embed: `<iframe src="https://play2048.co/" width="400" height="500" frameborder="0"></iframe>`
+  },
+  {
+    title: "Tetris",
+    description: "Stack the blocks. Classic challenge.",
+    thumbnail: "https://tetris.com/favicon.ico",
+    embed: `<iframe src="https://tetris.com/play-tetris" width="400" height="500" frameborder="0"></iframe>`
+  },
+  {
+    title: "HexGL",
+    description: "Futuristic 3D racing game.",
+    thumbnail: "https://hexgl.bkcore.com/favicon.ico",
+    embed: `<iframe src="https://hexgl.bkcore.com/" width="600" height="400" frameborder="0"></iframe>`
+  },
+  {
+    title: "Breakout",
+    description: "Destroy all the bricks!",
+    thumbnail: "https://www.retrogames.cz/games/568/favicon.png",
+    embed: `<iframe src="https://www.retrogames.cz/play_568-ATARI2600.php" width="600" height="400" frameborder="0"></iframe>`
+  }
+];
